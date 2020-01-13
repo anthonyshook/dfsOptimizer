a41f52a4af699f63e63596f4820c2da8fbb035c0
Anthony Shook <anthonyshook@gmail.com>
2020-01-13 15:40:24 -0600

    Added player lock and block constraint functions

--- R/constraints.R
+++ R/constraints.R
@@ -1,1 +1,24 @@
-#' Max Exposure Constraint
+#' Block Players Constraint
+#'
+#' @param model Model object
+#' @param block_vector Vector where 1 indicates 'block'
+#'
+#' @keywords internal
+add_block_constraint <- function(model, block_vector) {
+  blocks <- which(block_vector == 1)
+  model <- model %>%
+    ompr::add_constraint(players[i] == 0, i = blocks)
+}
+
+
+#' Lock Players Constraint
+#'
+#' @param model Model object
+#' @param lock_vector Vector where 1 indicates 'block'
+#'
+#' @keywords internal
+add_lock_constraint <- function(model, lock_vector) {
+  locks <- which(lock_vector == 1)
+  model <- model %>%
+    ompr::add_constraint(players[i] == 1, i = locks)
+}
