c6f52c282c913c9f98c5859905f46e1296f8f0f0
Anthony Shook <anthonyshook@gmail.com>
2020-01-14 02:30:26 -0600

    Moved flex positions to optimConfig

--- R/class-optim-model.R
+++ R/class-optim-model.R
@@ -6,22 +6,19 @@ setOldClass('milp_model')
 #'
 #' @slot mod The model object
 #' @slot solver Method for solving; currently only 'glpk' is available
-#' @slot flex_positions Character array of flex positions
 #'
 #' @export
 optim_model <- setClass('optimModel',
                         slots = list(
                           mod = 'milp_model',
-                          solver = 'character',
-                          flex_positions = 'character'
+                          solver = 'character'
                         ),
                         prototype = list(
                           mod = ompr::MILPModel(),
-                          solver = 'glpk',
-                          flex_positions = NA_character_
-                          ))
+                          solver = 'glpk'
+                        ))
 
 setMethod('show','optimModel', function(object) {
   cat(paste('Solver:', object@solver, '\n'))
   show(object@mod)
-  })
+})
