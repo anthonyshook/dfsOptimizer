6739d0efd1e0095d4bbafb9c0916b899749ffb70
Anthony Shook <anthonyshook@gmail.com>
2020-01-28 01:10:00 -0600

    Util function to get unique grid

--- R/utils.R
+++ R/utils.R
@@ -19,3 +19,50 @@ calculate_exposure <- function(veclist) {
   exposure <- (sum_vecs + 1) / (length(veclist) + 1)
 
 }
+
+# Expands grid with unique combinations
+# No repeats, no repetitions
+expand.grid.unique <- function(..., rep = FALSE, uniqueCombinations = TRUE) {
+
+  # All possible elements
+  dat <- data.table::CJ(..., unique = TRUE)
+
+  # Get possible combinations
+  matind     <- combn(colnames(dat), 2)
+  matchcheck <- 0
+  intercheck <- 0
+
+  # This compares the content of every column
+  # If EXACTLY the same sets of players are present in two columns,
+  # we remove half (the upper-right corner of the pairwise matrix)
+  for (I in 1:ncol(matind)) {
+    i <- matind[1, I]
+    j <- matind[2, I]
+
+    # Check for fully matching columns
+    if (length(setdiff(dat[[i]], dat[[j]])) == 0) {
+      # If there is no difference between columns, cut half out by inequality >
+      dat <- dat[dat[[j]] > dat[[i]]]
+    } else if (length(intersect(dat[[i]], dat[[j]])) > 0) {
+      # If there is a setdiff, AND and intersection,
+      # remove repeats by intersection
+      dat <- dat[dat[[i]] == intersect(dat[[i]], dat[[j]])]
+    }
+  }
+
+  # Note -- if NO matches are made, we should NOT have to remove repeats
+  # because the code should define it that way
+
+  # # Removes repetitions
+  # if (!rep) {
+  #   dat <- dat[apply(dat, 1, data.table::uniqueN) >= ncol(dat)]
+  # }
+  #
+  # # Reduce cases to unique combinations ()
+  # if (uniqueCombinations) {
+  #   dat <- dat[!duplicated(t(apply(as.matrix(dat), 1, sort))), ]
+  # }
+
+  return(dat)
+
+}
