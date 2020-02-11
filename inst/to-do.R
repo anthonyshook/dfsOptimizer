03ee0349616994dbadd31798dfe10ae9192e2f15
Anthony Shook <anthonyshook@gmail.com>
2020-02-11 00:22:04 -0600

    last to-do update tonight

--- inst/to-do.R
+++ inst/to-do.R
@@ -1,17 +1,17 @@
 
 #' TO DO
-#' - Methods for adding all the existing constraints!
-#' - Methods for Setting config fields?
+#' - Methods for Setting config fields?  (STILL NEED TO CONSIDER THE API)
 #' - FanDuel Import function
 #' - Yahoo Import Function
 #' - Import from data.frame function
 #' - Formatting for Lineup output (DK, FD, and Yahoo)
 #' - Add NBA
 #' - Add MLB
+#' - Add NFL
+#' - Add NASCAR
 #' - Test SHOWDOWN mode (the objective will have to change, for CAPTAIN mode. (value[1] * 1.5 + value[2:6] * 1), && budget[1]*1.5 + budget[2:6] * 1)
 #' - Consider replacing the current flex-positions process with a UTIL/FLEX position process that is more easily fixed.
 #'   - TRIED. doesn't work ATM because our position constraint is based on positions, so you can't have the final UTIL position == 1 if it's made up of the SUM of all others.  The way we're currently designed, it only works if UTIL is a specific position. NOTE -- you don't need position constraints if you have specific positions...
-#' - Consider replacing all the setter functions with straight `fnc<-` methods
 
 #' FEATURES TO ADD
 #' - Add Opposing -positive- constraints (if team A QB, then team B WR) [force_opposing_positions]
