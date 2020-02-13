830f276a17a22e997d25377a6ec7302fe6a1452e
Anthony Shook <anthonyshook@gmail.com>
2020-02-12 22:05:08 -0600

    Updated add_players_from CSV * added site argument with 'custom' option Method to add players to model from data.frame
    * Misc: More example fixing

--- R/methods-optimizer.R
+++ R/methods-optimizer.R
@@ -1,27 +1,68 @@
 #' @include class-optimizer.R
 
-setGeneric('add_players_from_csv', function(object, filepath) standardGeneric('add_players_from_csv'))
+setGeneric('add_players_from_csv', function(object, filepath, site) standardGeneric('add_players_from_csv'))
 #' Add players to optimizer from CSV
 #'
 #' @param object An optimizer model object
 #' @param filepath Filepath location of the CSV
+#' @param site The site from which the CSV was obtained. If not provided, the site is inferred from
+#'    the model object. For user-generated CSVs with player data, one can set this argument to 'CUSTOM',
+#'    and the data will be parsed by \code{get_players_from_date_frame}
 #'
 #' @return Optimizer model object with slot \code{players} filled
 #'
 #' @examples
 #' \dontrun{
-#' opt <- optimizer(site = 'DRAFTKINGs', sport = 'HOCKEY', contest_type = 'CLASSIC')
+#' opt <- create_optimizer(site = 'DRAFTKINGs', sport = 'HOCKEY', contest_type = 'CLASSIC')
 #' opt <- add_players_from_csv(object = opt, filepath = '/Path/to/file.csv')
 #' }
 #'
 #' @export
 setMethod(f = 'add_players_from_csv',
           signature = 'optimizer',
-          definition = function(object, filepath) {
+          definition = function(object, filepath, site) {
+
+            if (missing('site')) {
+              site = object@site
+            }
+
             # Read in the players and put them in the players slot
-            object@players <- get_players_from_csv(path = filepath, site = object@site)
+            object@players <- get_players_from_csv(path = filepath, site = toupper(site))
             return(object)
           })
+
+
+setGeneric('add_players_from_df', function(object, df) standardGeneric('add_players_from_df'))
+#' Add players to optimizer from a data.frame
+#'
+#' @param object An optimizer model object
+#' @param df a data.frame of players to add to the model
+#'
+#' @return Optimizer model object with slot \code{players} filled
+#'
+#' @examples
+#' \dontrun{
+#' opt <- create_optimizer(site = 'DRAFTKINGs', sport = 'HOCKEY', contest_type = 'CLASSIC')
+#' dat <- data.frame(first_name = c('Sidney','Alex'),
+#'                  last_name = c('Crosby','Ovechkin'),
+#'                  fpts = c(18,20),
+#'                  team = c('PIT','WAS'),
+#'                  position = c('C','W'),
+#'                  salary = c(8000,9000),
+#'                  game_info = c('this will be','ignored anyway'),
+#'                  max_exposure = c(0.3, 0.6))
+#' opt <- add_players_from_df(object = opt, df = dat)
+#' }
+#'
+#' @export
+setMethod(f = 'add_players_from_df',
+          signature = 'optimizer',
+          definition = function(object, df) {
+            # Read in the players and put them in the players slot
+            object@players <- get_players_from_data_frame(df = df)
+            return(object)
+          })
+
 
 
 setGeneric('add_team_stack', function(object, positions) standardGeneric('add_team_stack'))
@@ -34,7 +75,7 @@ setGeneric('add_team_stack', function(object, positions) standardGeneric('add_te
 #'
 #' @examples
 #' \dontrun{
-#' opt <- optimizer(site = 'DRAFTKINGs', sport = 'HOCKEY', contest_type = 'CLASSIC')
+#' opt <- create_optimizer(site = 'DRAFTKINGs', sport = 'HOCKEY', contest_type = 'CLASSIC')
 #' opt <- add_players_from_csv(object = opt, filepath = '/Path/to/file.csv')
 #'
 #' # Add team stack, requiring a Center and Two Wingers from the same team
@@ -72,7 +113,7 @@ setGeneric('restrict_opposing_positions', function(object, pos1, pos2) standardG
 #'
 #' @examples
 #' \dontrun{
-#' opt <- optimizer(site = 'DRAFTKINGs', sport = 'HOCKEY', contest_type = 'CLASSIC')
+#' opt <- create_optimizer(site = 'DRAFTKINGs', sport = 'HOCKEY', contest_type = 'CLASSIC')
 #' opt <- add_players_from_csv(object = opt, filepath = '/Path/to/file.csv')
 #'
 #' # Restrict lineup from having Skaters and Goalies from opposite teams
@@ -131,7 +172,7 @@ setGeneric('set_flex_positions', function(object, positions) standardGeneric('se
 #'
 #' @examples
 #' \dontrun{
-#' opt <- optimizer(site = 'DRAFTKINGs', sport = 'HOCKEY')
+#' opt <- create_optimizer(site = 'DRAFTKINGs', sport = 'HOCKEY')
 #' opt <- add_players_from_csv(object = opt, filepath = '/Path/to/file.csv')
 #'
 #' # Default FLEX is c('C','W','D') -- here we'll remove D
