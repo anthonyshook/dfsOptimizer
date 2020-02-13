4f82761ba9dce82c5f1684bcfd17884ad3a654f5
Anthony Shook <anthonyshook@gmail.com>
2020-02-13 02:20:25 -0600

    added some base orders

---
+++ R/base-orders.R
@@ -0,0 +1,31 @@
+dk_orders <- list(
+  HOCKEY = list(
+    CLASSIC = c('C', 'C', 'W', 'W', 'W', 'D', 'D', 'G', 'UTIL'),
+    SHOWDOWN = c(),
+    TIERS = c()
+  )
+)
+
+
+fd_orders <- list(
+  HOCKEY = list(
+    CLASSIC = c('C', 'C', 'W', 'W', 'W', 'D', 'D', 'G', 'UTIL'),
+    SHOWDOWN = c(),
+    TIERS = c()
+  )
+)
+
+
+yahoo_orders <- list(
+  HOCKEY = list(
+    CLASSIC = c('C', 'C', 'W', 'W', 'W', 'D', 'D', 'G', 'UTIL'),
+    SHOWDOWN = c(),
+    TIERS = c()
+  )
+)
+
+base_orders <- list(
+  DRAFTKINGS = dk_orders,
+  FANDUEL = fd_orders,
+  YAHOO = yahoo_orders
+)
