0ea5359b9b270c418791b863a46d72378791fcbe
Anthony Shook <anthonyshook@gmail.com>
2020-02-19 02:07:40 -0600

    added parse_roster_key

--- R/lineup-utils.R
+++ R/lineup-utils.R
@@ -29,3 +29,34 @@ reorder_lineup <- function(lineup, new_order) {
 
   return(lineup)
 }
+
+#' Parse roster key item
+#'
+#' @param rk Roster key from base_settings
+#'
+#' @details This function will take all single and multiplayer slots from the roster key, and
+#'     build a table containing mins/maxes for each position.  These are then used to set the
+#'     value of how many of each position is allowable in a lineup.
+#'
+#' @keywords internal
+parse_roster_key <- function(rk) {
+
+  # lapply through roster key
+  by_row <- lapply(rk, function(i) {
+    if (length(i$positions)==1) {
+      # Set max and min to the number of players for that position
+      out <- data.frame(pos = i$positions, min = i$num, max = i$num)
+    } else if (length(i$positions > 1)) {
+      # Set max to the number of players for those positions leaving min as 0
+      out <- data.frame(pos = i$positions, min = 0, max = i$num)
+    } else {
+      # Break because it means there was no position
+      stop("Length of 'positions' is less than 1!")
+    }
+  })
+
+  # Sum the mins and maxes up by position
+  sum_table <- data.table::rbindlist(by_row)[, .(min = sum(min), max = sum(max)), by = pos]
+
+  return(sum_table)
+}
