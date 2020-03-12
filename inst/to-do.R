ce460e3bfd70bd797be87743fd21acb0ef815cca
Anthony Shook <anthonyshook@gmail.com>
2020-03-12 01:35:01 -0500

    to-do update

--- inst/to-do.R
+++ inst/to-do.R
@@ -3,16 +3,15 @@
 #' - Methods for Setting config fields?  (STILL NEED TO CONSIDER THE API -slightly less important now)
 #' - Build LINEUP class to manage lineup objects [determine if necessary... useful for passing back to model]
 #' - Test SHOWDOWN mode (the objective will have to change, for CAPTAIN mode. (value[1] * 1.5 + value[2:6] * 1), && budget[1]*1.5 + budget[2:6] * 1)
-#' - Add base constraints to the constraint list of the config?? (instead of in 'construct model')
 
 
 #' FEATURES TO ADD
 #' - Add Opposing -positive- constraints (if team A QB, then team B WR) [force_opposing_positions]
-#' - Add grouped-stack constraints (Line matching for hockey, depth order for baseball...) [would be easier with sport/site/contest_type based subclasses?]
 #' - Add method for specifying optional stack elements (like QB and *ONE OF* WR / TE -- possibly changing the input structure from single vector to list)
 #' - Max repeating players (code exists, function to set it does not)
 #' - Consider adding variance as a player-level option (perhaps with min/max variance options.)
 #' - Add functionality to include predefined lineups (Mostly useful when randomness is included, or the model will likely produce exactly the same values).
+#' - Add grouped-stack constraints (Line matching for hockey, depth order for baseball...) [would be easier with sport/site/contest_type based subclasses?]
 
 #' SEMI LONG-TERM TO DO
 #' - write tests
