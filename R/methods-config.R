44ffe68076f9236750a64695b6abfa59fce9ccd7
Anthony Shook <anthonyshook@gmail.com>
2020-03-12 01:35:22 -0500

    standardized setting at config level

--- R/methods-config.R
+++ R/methods-config.R
@@ -27,10 +27,8 @@ setMethod("max_exposure", "optimConfig", function(x) x@max_exposure)
 setGeneric("variance", function(x) standardGeneric("variance"))
 setMethod("variance", "optimConfig", function(x) x@variance)
 
-setGeneric("min_budget", function(x) standardGeneric("min_budget"))
-setMethod("min_budget", "optimConfig", function(x) x@min_budget)
-
 ### Setter Methods
+## All setters have the 'set_' prefix, and those generics are referenced in methods-optimizer
 setGeneric('set_max_budget<-', function(x, value) standardGeneric('set_max_budget<-'))
 setMethod('set_max_budget<-', 'optimConfig', function(x, value) {
   x@budget <- value
@@ -45,8 +43,8 @@ setMethod('set_min_budget<-', 'optimConfig', function(x, value) {
   return(x)
 })
 
-setGeneric('roster_size<-', function(x, value) standardGeneric('roster_size<-'))
-setMethod('roster_size<-', 'optimConfig', function(x, value) {
+setGeneric('set_roster_size<-', function(x, value) standardGeneric('set_roster_size<-'))
+setMethod('set_roster_size<-', 'optimConfig', function(x, value) {
   x@roster_size <- value
   stopifnot(validObject(x))
   return(x)
@@ -66,22 +64,22 @@ setMethod('set_max_players_per_team<-', 'optimConfig', function(x, value) {
   return(x)
 })
 
-setGeneric('flex_position<-', function(x, value) standardGeneric('flex_position<-'))
-setMethod('flex_position<-', 'optimConfig', function(x, value) {
+setGeneric('set_flex_position<-', function(x, value) standardGeneric('set_flex_position<-'))
+setMethod('set_flex_position<-', 'optimConfig', function(x, value) {
   x@flex_position <- value
   stopifnot(validObject(x))
   return(x)
 })
 
-setGeneric('max_exposure<-', function(x, value) standardGeneric('max_exposure<-'))
-setMethod('max_exposure<-', 'optimConfig', function(x, value) {
+setGeneric('set_max_exposure<-', function(x, value) standardGeneric('set_max_exposure<-'))
+setMethod('set_max_exposure<-', 'optimConfig', function(x, value) {
   x@max_exposure <- value
   stopifnot(validObject(x))
   return(x)
 })
 
-setGeneric('variance<-', function(x, value) standardGeneric('variance<-'))
-setMethod('variance<-', 'optimConfig', function(x, value) {
+setGeneric('set_variance<-', function(x, value) standardGeneric('set_variance<-'))
+setMethod('set_variance<-', 'optimConfig', function(x, value) {
   x@variance <- value
   stopifnot(validObject(x))
   return(x)
