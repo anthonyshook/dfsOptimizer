8c0f009be33db35eeb5453b94f7dac4f749210a1
Anthony Shook <anthonyshook@gmail.com>
2020-03-26 01:33:32 -0500

    to-do update

--- inst/to-do.R
+++ inst/to-do.R
@@ -9,10 +9,10 @@
 #' - Test SHOWDOWN mode (the objective will have to change, for CAPTAIN mode. (value[1] * 1.5 + value[2:6] * 1), && budget[1]*1.5 + budget[2:6] * 1)
 
 #' FEATURES TO ADD
-#' - Add Opposing -positive- constraints (if team A QB, then team B WR) [force_opposing_positions]
-#' - Add method for specifying optional stack elements (like QB and *ONE OF* WR / TE -- possibly changing the input structure from single vector to list)
 #' - Consider adding variance as a player-level option (perhaps with min/max variance options.)
+#' - Add method for specifying optional stack elements (like QB and *ONE OF* WR / TE -- possibly changing the input structure from single vector to list)
 #' - Add grouped-stack constraints (Line matching for hockey, depth order for baseball...) [would be easier with sport/site/contest_type based subclasses?]
+#'   - Could see this as a V1.2...
 
 
 
@@ -27,7 +27,7 @@
 #' - Add friendly error reporting for things like "You used a CSV I have never seen", etc.
 #' - Refactor the whole damn stacking code, potentially.  We may want to check out CVXR and see if that is better from a speed of construction perspective.
 #'   - If there is a WAY to use the pre-defined player-stacks, that would be the ideal
-#'
+#' - Make `Force Opposing` restriction capable of taking more than just one position per team
 
 #' NICE TO HAVE IMPROVEMENTS
 #' - fix the date parsing for non-draftkings
