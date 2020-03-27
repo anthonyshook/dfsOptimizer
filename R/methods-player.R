06d3a188bf97e2834735f5395b4a4d5fd34382ec
Anthony Shook <anthonyshook@gmail.com>
2020-03-26 20:54:40 -0500

    Variance accessors exported more functions

--- R/methods-player.R
+++ R/methods-player.R
@@ -1,35 +1,44 @@
 
 # Accessor functions
+#' @ export
 setGeneric("fpts", function(x) standardGeneric("fpts"))
 setMethod("fpts", "player_object", function(x) x@fpts)
 
+#' @ export
 setGeneric("id", function(x) standardGeneric("id"))
 setMethod("id", "player_object", function(x) x@id)
 
+#' @ export
 setGeneric("fullname", function(x) standardGeneric("fullname"))
 setMethod("fullname", "player_object", function(x) x@fullname)
 
+#' @ export
 setGeneric("team", function(x) standardGeneric("team"))
 setMethod("team", "player_object", function(x) x@team)
 
+#' @ export
 setGeneric("position", function(x) standardGeneric("position"))
 setMethod("position", "player_object", function(x) x@position)
 
+#' @ export
 setGeneric("salary", function(x) standardGeneric("salary"))
 setMethod("salary", "player_object", function(x) x@salary)
 
 #setGeneric("max_exposure", function(x) standardGeneric("max_exposure"))
 # Generic already set in class-config
+#' @ export
 setMethod("max_exposure", "player_object", function(x) x@max_exposure)
 
+#' @ export
 setGeneric("blocked", function(x) standardGeneric("blocked"))
 setMethod("blocked", "player_object", function(x) as.numeric(x@blocked))
 
+#' @ export
 setGeneric("locked", function(x) standardGeneric("locked"))
 setMethod("locked", "player_object", function(x) as.numeric(x@locked))
 
+#' @ export
 setGeneric('get_opposing_team', function(x) standardGeneric('get_opposing_team'))
-#' @export
 setMethod('get_opposing_team', 'player_object',
           function(x){
             opponent <- setdiff(c(home_team(x@game_info), away_team(x@game_info)),
@@ -37,6 +46,11 @@ setMethod('get_opposing_team', 'player_object',
             return(opponent)
 
           })
+
+#' @ export
+setGeneric('variance', function(x) standardGeneric('variance'))
+setMethod('variance', 'player_object', function(x) x@variance)
+
 
 # Formatter method
 setMethod('get_player_data', 'player_object',
@@ -52,6 +66,7 @@ setMethod('get_player_data', 'player_object',
 
 
 # Update methods
+#' @ export
 setGeneric('lock_player', function(object) standardGeneric('lock_player'))
 setMethod('lock_player', 'player_object',
           function(object) {
@@ -60,6 +75,8 @@ setMethod('lock_player', 'player_object',
             return(object)
           })
 
+
+#' @ export
 setGeneric('block_player', function(object) standardGeneric('block_player'))
 setMethod('block_player', 'player_object',
           function(object) {
@@ -68,6 +85,8 @@ setMethod('block_player', 'player_object',
             return(object)
           })
 
+
+#' @ export
 setGeneric('set_min_exposure', function(object, exposure) standardGeneric('set_min_exposure'))
 setMethod('set_min_exposure', 'player_object',
           function(object, exposure){
@@ -78,7 +97,9 @@ setMethod('set_min_exposure', 'player_object',
             return(object)
           })
 
+
 # setGeneric('set_max_exposure', function(object, exposure) standardGeneric('set_max_exposure'))
+#' @ export
 setMethod('set_max_exposure', 'player_object',
           function(object, exposure){
             if (exposure > 1 || exposure < 0) {
@@ -88,6 +109,8 @@ setMethod('set_max_exposure', 'player_object',
             return(object)
           })
 
+
+#' @ export
 setGeneric('set_as_injured', function(object, exposure) standardGeneric('set_as_injured'))
 setMethod('set_as_injured', 'player_object',
           function(object){
@@ -95,6 +118,8 @@ setMethod('set_as_injured', 'player_object',
             return(object)
           })
 
+
+#' @ export
 setGeneric('set_as_active', function(object, exposure) standardGeneric('set_as_active'))
 setMethod('set_as_active', 'player_object',
           function(object){
@@ -103,12 +128,17 @@ setMethod('set_as_active', 'player_object',
           })
 
 
+#' @ export
 setGeneric('set_fpts', function(object, pts) standardGeneric('set_fpts'))
 setMethod('set_fpts', 'player_object',
           function(object, pts) {
             object@fpts <- pts
             return(object)
           })
+
+#' @ export
+setGeneric('set_variance', function(object, variance) standardGeneric('set_variance'))
+setMethod('set_variance', 'player_object', function(object, variance) object@variance <- variance)
 
 #setGeneric('apply_variance', function(object, varpct) standardGeneric('apply_variance'))
 setMethod('apply_variance', 'player_object',
