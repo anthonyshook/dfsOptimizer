ccbc0fdd8e947e229b766ec7eb70882c91222c74
Anthony Shook <anthonyshook@gmail.com>
2020-04-06 01:23:11 -0500

    Added lineup class with summary show and subclasses

---
+++ R/class-lineups.R
@@ -0,0 +1,45 @@
+#' S4 Lineup class
+#'
+#' @slot num_lineups Number of lineups
+#' @slot lineups A list of lineups
+#'
+setClass('lineupClass',
+         slots = list(
+           num_lineups = 'numeric',
+           lineups = 'list'
+         ),
+         contains = 'VIRTUAL')
+
+
+# Show method
+setMethod('show', 'lineupClass', function(object) {
+  # Just print lineups to screen
+  completed_lineups <- object@lineups[!sapply(object@lineups, is.null)]
+  if (length(completed_lineups) > 0) {
+    print(completed_lineups)
+  } else {
+    print("No complete lineups found!")
+  }
+})
+
+# Summary for lineupClass
+summary.lineupClass <- function(object) {
+  nonnull_lineups <- object@lineups[!sapply(object@lineups, is.null)]
+  cat(paste0('Number of Expected Lineups: ', length(object@lineups),'\n'))
+  cat(paste0('Number of Lineups Found: ', length(nonnull_lineups), '\n\n'))
+
+  if (length(nonnull_lineups) > 0) {
+    # Then we'll need to do something, but for now
+    cat("Player Exposures:\n")
+    smry <- get_player_summary(nonnull_lineups)
+    print(smry)
+    cat("\nPlayers Per Team:")
+    print(get_team_summary(nonnull_lineups))
+  }
+
+}
+
+
+# Subclasses
+setClass('lineupClassic', contains = 'lineupClass')
+setClass('lineupSingle', contains = 'lineupClass')
