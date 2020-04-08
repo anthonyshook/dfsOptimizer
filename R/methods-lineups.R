7e6cf7e1262ec08eeb449cdfb767a1461d4bf3a6
Anthony Shook <anthonyshook@gmail.com>
2020-04-08 00:56:31 -0500

    Added format_for_upload and export_lineups methods

--- R/methods-lineups.R
+++ R/methods-lineups.R
@@ -9,3 +9,37 @@ setMethod('get_player_exposures', 'lineupClass',
             return(get_player_summary(object@lineups))
           })
 
+
+setGeneric('format_for_upload', function(object) standardGeneric('format_for_upload'))
+#' Function for getting lineups into uploadable format
+#'
+#' @param object An object of class Lineups
+#'
+#' @export
+setMethod('format_for_upload', 'lineupClass',
+          function(object) {
+            ids <- lapply(object@lineups, function(i) {
+              o <- data.frame(t(i$id))
+              names(o) <- i$roster_position
+              return(o)
+            })
+            return(data.table::rbindlist(ids))
+          })
+
+
+setGeneric('export_lineups', function(object, file = "lineup_export.csv") standardGeneric('export_lineups'))
+#' @title Export Lineups to CSV
+#'
+#' @param object An object of class Lineups
+#' @param file A character string indicating the file to write to
+#'
+#' @export
+setMethod('export_lineups', 'lineupClass',
+          function(object, file = 'lineup_export.csv') {
+            # Format
+            flines <- format_for_upload(object)
+            # Write out using data.table
+            data.table::fwrite(flines, file = file)
+          })
+
+
