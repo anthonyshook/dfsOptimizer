80ea32b150305c156ab0d6a10e507baac6f5d3ef
Anthony Shook <anthonyshook@gmail.com>
2021-01-22 13:58:25 -0600

    Initial ghpages commit

--- R/imports.R
+++
@@ -1,7 +0,0 @@
-#' @importFrom magrittr %>%
-#' @importFrom data.table :=
-#' @importFrom ompr colwise
-#' @importFrom ompr sum_expr
-#' @importFrom methods new slot slotNames validObject
-#' @import ROI.plugin.glpk
-NULL
